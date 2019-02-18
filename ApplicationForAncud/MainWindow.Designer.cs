417a1c7fddae966309c25bc947cf51c91cffced8
ilya_serebryakov_00@mail.ru <ilya_serebryakov_00@mail.ru>
2019-02-18 21:38:08 +0300

    Добавьте файлы проекта.

---
+++ ApplicationForAncud/MainWindow.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ApplicationForAncud
+{
+    partial class MainWindow
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        private void InitializeComponent()
+        {
+            this.dgvFileCRC = new System.Windows.Forms.DataGridView();
+            this.File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CRC32 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFileCRC)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvFileCRC
+            // 
+            this.dgvFileCRC.AllowUserToAddRows = false;
+            this.dgvFileCRC.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFileCRC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFileCRC.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.File,
+            this.CRC32});
+            this.tableLayoutPanel1.SetColumnSpan(this.dgvFileCRC, 3);
+            this.dgvFileCRC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFileCRC.Location = new System.Drawing.Point(3, 3);
+            this.dgvFileCRC.Name = "dgvFileCRC";
+            this.dgvFileCRC.RowHeadersVisible = false;
+            this.dgvFileCRC.Size = new System.Drawing.Size(659, 284);
+            this.dgvFileCRC.TabIndex = 0;
+            // 
+            // File
+            // 
+            this.File.HeaderText = "File";
+            this.File.Name = "File";
+            // 
+            // CRC32
+            // 
+            this.CRC32.HeaderText = "CRC32";
+            this.CRC32.Name = "CRC32";
+            // 
+            // addButton
+            // 
+            this.addButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addButton.Location = new System.Drawing.Point(468, 293);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(94, 30);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.deleteButton.Location = new System.Drawing.Point(568, 293);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(94, 30);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvFileCRC, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.deleteButton, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.addButton, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(665, 326);
+            this.tableLayoutPanel1.TabIndex = 3;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 326);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainWindow";
+            this.Text = "Application for Ancud";
+            this.Load += new System.EventHandler(this.AppForAncud_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFileCRC)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFileCRC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CRC32;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ ApplicationForAncud/MainWindow.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApplicationForAncud
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+
+            OPF.Filter = "jpg|*.jpg|txt|*.txt";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                int rowNumber = dgvFileCRC.Rows.Add();
+
+                dgvFileCRC.Rows[rowNumber].Cells[0].Value = OPF.FileName;
+                dgvFileCRC.Rows[rowNumber].Cells[1].Value = CRCTools.CalculateCRC(OPF.FileName);
+                MessageBox.Show("File " + OPF.FileName + " added.", "Add", MessageBoxButtons.OK);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dgvFileCRC.CurrentRow == null)
+            {
+                MessageBox.Show("Choose row!");
+                return;
+            }
+
+            int deleteNumber = dgvFileCRC.SelectedCells[0].RowIndex;
+            string fileName = dgvFileCRC.SelectedCells[0].Value.ToString();
+
+            dgvFileCRC.Rows.RemoveAt(deleteNumber);
+            MessageBox.Show("File " + fileName + " deleted.", "Delete", MessageBoxButtons.OK);
+        }
+
+        private void AppForAncud_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    class CRCTools
+    {
+        static uint[] tableCRC32;
+
+        static CRCTools()
+        {
+            InitTableCRC32();
+        }
+
+        static void InitTableCRC32()
+        {
+            tableCRC32 = new uint[256];
+            const uint POLYNOMIAL = 0xEDB88320;
+            uint crc32;
+
+            for (int i = 0; i < 256; i++)
+            {
+                crc32 = (uint)i;
+
+                for (int j = 8; j > 0; j--)
+                {
+                    if ((crc32 & 1) == 1)
+                        crc32 = (crc32 >> 1) ^ POLYNOMIAL;
+                    else
+                        crc32 >>= 1;
+                }
+        
+                tableCRC32[i] = crc32;
+            }
+        }
+
+        public static uint CalculateCRC(string fileName)
+        {
+            const int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            uint result = 0xFFFFFFFF;
+            System.IO.FileStream stream = System.IO.File.OpenRead(fileName);
+            int count = stream.Read(buffer, 0, bufferSize);
+
+            while (count > 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    result = ((result) >> 8)
+                        ^ tableCRC32[(buffer[i])
+                        ^ ((result) & 0x000000FF)];
+                }
+
+                count = stream.Read(buffer, 0, bufferSize);
+            }
+
+            return ~result;
+        }
+    }
+}
