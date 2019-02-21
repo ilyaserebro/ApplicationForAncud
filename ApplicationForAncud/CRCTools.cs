edefa4c59bc4f42755c4be1ef023e737d8ae0e02
ilya_serebryakov_00@mail.ru <ilya_serebryakov_00@mail.ru>
2019-02-21 18:43:38 +0300

    Delete unused using and move CRCTool to single file

---
+++ ApplicationForAncud/CRCTools.cs
@@ -0,0 +1,57 @@
+﻿namespace ApplicationForAncud
+{
+    class CRCTools
+    {
+        static uint[] tableCRC32;
+
+        static CRCTools()
+        {
+            InitTableCRC32();
+        }
+
+        static void InitTableCRC32()
+        {
+            tableCRC32 = new uint[256];
+            const uint POLYNOMIAL = 0xEDB88320;
+            uint crc32;
+
+            for (int i = 0; i < 256; i++)
+            {
+                crc32 = (uint)i;
+
+                for (int j = 8; j > 0; j--)
+                {
+                    if ((crc32 & 1) == 1)
+                        crc32 = (crc32 >> 1) ^ POLYNOMIAL;
+                    else
+                        crc32 >>= 1;
+                }
+
+                tableCRC32[i] = crc32;
+            }
+        }
+
+        public static uint CalculateCRC(string fileName)
+        {
+            const int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            uint result = 0xFFFFFFFF;
+            System.IO.FileStream stream = System.IO.File.OpenRead(fileName);
+            int count = stream.Read(buffer, 0, bufferSize);
+
+            while (count > 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    result = ((result) >> 8)
+                        ^ tableCRC32[(buffer[i])
+                        ^ ((result) & 0x000000FF)];
+                }
+
+                count = stream.Read(buffer, 0, bufferSize);
+            }
+
+            return ~result;
+        }
+    }
+}
